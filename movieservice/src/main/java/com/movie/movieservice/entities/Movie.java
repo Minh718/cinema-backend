f1fde2833dd2cdfb0756eaca88795bd16d8eadb2
Minh718 <minhprokute85209@gmail.com>
2025-07-08 07:26:06 +0700

    chore(movie-service): initialize Spring Boot service

---
+++ movieservice/src/main/java/com/movie/movieservice/entities/Movie.java
@@ -0,0 +1,49 @@
+package com.movie.movieservice.entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private Integer duration;
+    private String director;
+    private String language;
+    private String genre;
+    private String thumbnailUrl;
+    private LocalDate releaseDate;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
