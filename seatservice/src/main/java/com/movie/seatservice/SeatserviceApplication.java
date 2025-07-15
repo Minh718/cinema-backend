d25789111d111eb3e95a980bcd5aaf2898d7b813
Minh718 <minhprokute85209@gmail.com>
2025-07-15 14:02:23 +0700

    Chore(seat-service): initialize Spring Boot service

---
+++ seatservice/src/main/java/com/movie/seatservice/SeatserviceApplication.java
@@ -0,0 +1,13 @@
+package com.movie.seatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatserviceApplication.class, args);
+	}
+
+}
---
+++ seatservice/src/test/java/com/movie/seatservice/SeatserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.seatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
