ab450f87e911d127e4dfe89028d45c8b3f9e77fc
Minh718 <minhprokute85209@gmail.com>
2025-07-17 08:33:41 +0700

    chore(heatseat-service): initialize Spring Boot service

---
+++ heatseatservice/src/main/java/com/movie/heatseatservice/HeatseatserviceApplication.java
@@ -0,0 +1,13 @@
+package com.movie.heatseatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatseatserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatseatserviceApplication.class, args);
+	}
+
+}
---
+++ heatseatservice/src/test/java/com/movie/heatseatservice/HeatseatserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.heatseatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatseatserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
